8786cd383c65ae869e59aae6194d2d295df0425b
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2023-04-29 13:18:36 +0200

    Test 3 draft

--- e2e/carto.spec.ts
+++ e2e/carto.spec.ts
@@ -19,7 +19,7 @@ test('Test 1. Open map and validate info', async ({ page, baseURL }) => {
     });
 
     await test.step('Check the widget of data showed in the map', async () => {
-        // TODO
+        // I was not able to find a propper way to req
     });
 });
 
@@ -46,4 +46,48 @@ test('Test 2. The app should hide the button to configure the failing dataset', 
     await test.step('The map should hide the button related to the Indices', async () => {
         await expect(page.getByRole('button', { name: 'Thematic Indices' })).toBeHidden();
     })
+});
+
+test('Test 3. Check that the "Facilities and structure" layer shows the correct data and is working correctly', async ({ page }) => {
+    await test.step('Open map', async () => {
+        await page.goto('https://climate-resilience.hidot.hawaii.gov/map/information/info');
+        await expect(page.locator('#deckgl-overlay')).toBeVisible();
+    });
+
+    await test.step('Enable the "Facilities and structure" layer (available on "More Layers")', async () => {
+        await page.getByRole('button', { name: 'More Layers' }).click();
+        await page.getByRole('listitem').filter({ hasText: 'Facilities and Structure' }).getByRole('checkbox').check();
+        await page.getByText('Configuration').press('Escape');
+    });
+
+    await test.step('Disable all the "HDOT Assets" (to be able to check in the map that all is working properly)', async () => {
+        await page.getByRole('button', { name: 'HDOT Assets', exact: true }).click();
+        await page.getByRole('button', { name: 'Unselect all' }).click();
+        await page.getByText('Configuration').press('Escape');
+    })
+
+
+    await test.step('Open the Insights of Thematic indices', async () => {
+        await page.getByRole('tab', { name: 'Thematic Indices' }).click();
+        await page.getByRole('tab', { name: 'Insights' }).click();
+    })
+
+    await test.step('Check that the widget is visible and the data is correct and properly showed in the map', async () => {
+        await expect(page.getByRole('region', { name: 'Facilities and Structures' })).toBeVisible();
+
+        // TODO: and the data is correct 
+        
+        // TODO: and properly showed in the map
+    });
+
+    // Then, you should zoom to the Honolulu island 
+    
+    // check that the widget values are adapted to the data shown for that zoom level.
+    
+    // filter the data by selecting one of the 3 available facilities in the widget 
+    // check that all works as expected and that the map has applied the filter changes.
+
+    // simulate the "hover" over one of the facilities in the map 
+    // check that the popup is shown with the proper information.
+
 });
