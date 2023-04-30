b50497d0f6a25f5feb6a6a15344f78835c5dc783
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2023-04-30 21:11:58 +0200

    Completed test 4

--- e2e/carto.spec.ts
+++ e2e/carto.spec.ts
@@ -131,10 +131,10 @@ test('Test 3. Check that the "Facilities and structure" layer shows the correct 
         // However, it sould be great to have attribute or JS variable set when the data is loaded.
         await page.getByRole('button', { name: 'HDOT Assets', exact: true }).click();
 
-        const menuEl = page.getByRole('menu').filter({has: page.getByRole('heading', { name: 'Configuration' })});
+        const menuEl = page.getByRole('menu').filter({ has: page.getByRole('heading', { name: 'Configuration' }) });
         const checkboxes = await menuEl.getByRole('checkbox').all();
-        for(const checkbox of checkboxes) {
-            await expect(checkbox).toBeEnabled({timeout: 60 * 1000});
+        for (const checkbox of checkboxes) {
+            await expect(checkbox).toBeEnabled({ timeout: 60 * 1000 });
         }
 
         await page.getByRole('button', { name: 'Unselect all' }).click();
@@ -174,9 +174,9 @@ test('Test 3. Check that the "Facilities and structure" layer shows the correct 
         await expect(page.locator('#deckgl-wrapper')).toHaveScreenshot();
     });
 
-    await test.step('Zoom to the Honolulu island',async () => {
+    await test.step('Zoom to the Honolulu island', async () => {
 
-         // The starting coordinates for the mouse (approx. center of the Honolulu island)
+        // The starting coordinates for the mouse (approx. center of the Honolulu island)
         let startX = 750;
         let startY = 120;
 
@@ -185,10 +185,10 @@ test('Test 3. Check that the "Facilities and structure" layer shows the correct 
         const endX = canvasBox.left + (canvasBox.width / 2);
         const endY = canvasBox.top + (canvasBox.height / 2);
 
-        await page.mouse.move(startX, startY, {steps: 5});
+        await page.mouse.move(startX, startY, { steps: 5 });
         await page.mouse.down();
-        await page.mouse.move(endX, endY, {steps: 5});
-        await page.mouse.up();    
+        await page.mouse.move(endX, endY, { steps: 5 });
+        await page.mouse.up();
 
 
         // Zooming 
@@ -200,10 +200,10 @@ test('Test 3. Check that the "Facilities and structure" layer shows the correct 
         startX = 750;
         startY = 250;
 
-        await page.mouse.move(startX, startY, {steps: 5});
+        await page.mouse.move(startX, startY, { steps: 5 });
         await page.mouse.down();
-        await page.mouse.move(endX, endY, {steps: 5});
-        await page.mouse.up();    
+        await page.mouse.move(endX, endY, { steps: 5 });
+        await page.mouse.up();
 
         await expect(page.locator('#deckgl-wrapper')).toHaveScreenshot();
     });
@@ -241,18 +241,80 @@ test('Test 3. Check that the "Facilities and structure" layer shows the correct 
 });
 
 test('Test 4. Main page navigation', async ({ page, baseURL }) => {
+    class TabInfo {
+        tabName: string;
+        sectionName: string;
+        id: string;
+
+        constructor(tabName: string, sectionName: string, id: string) {
+            this.tabName = tabName;
+            this.sectionName = sectionName;
+            this.id = id;
+        }
+    }
+
+    const tabs = [
+        new TabInfo('Climate resilience', 'Climate resilience', 'climate-resilience'),
+        // There are two elements with id = action-plan, which make HTML invalid
+        new TabInfo('Action Plan', 'The HDOT Action Plan', 'action-plan'),
+        new TabInfo('Climate stressor', 'Climate stressor', 'climate-stressor'),
+        new TabInfo('The Urgency', 'The Urgency', 'the-urgency'),
+        new TabInfo('HDOT Map', 'HDOT Asset and Hazard Map', 'explore-map'),
+        new TabInfo('Map components', 'Map components', 'map-components'),
+    ]
+
+    const goToTab = async (tab: TabInfo) => {
+        await page.getByRole('tab', { name: tab.tabName }).click();
+
+        const section = page.locator(`#${tab.id}.MuiGrid-root`);
+        await expect(section).toBeInViewport();
+        await expect(section.getByRole('heading').first()).toHaveText(tab.sectionName);
+    }
+
     await test.step(`Load the page ${baseURL}`, async () => {
         await page.goto('/');
     });
 
-    //  use the navigation bar to go to each entry going back to the start of the page between navigating to each section. 
-    // You should check that the section's name is the proper one when navigating it.
+    await test.step(`Use the navigation bar to go to each entry and check that secction name is propper`, async () => {
+        for (const tab of tabs) {
+            await goToTab(tab)
 
-    // After that, the test should go to the first section again and then directly to the second one.
+            // Somehow `await page.evaluate(() => window.scrollTo(0, 0));` does not work, 
+            // so I decided to go to the top with click on logo 
+            await page.locator('a').filter({ hasText: 'Hawai\'i HighwaysClimate Insights for Infrastructure' }).click();
+            await expect(page.locator('#back-to-top-anchor')).toBeInViewport();
+        }
 
-    // After that, we should go to the "Explore Map" section 
-    // check that, if we click the logo, we get back to the homepage. 
+    });
 
-    // Also, we would like to go back to the map 
-    // check that, if we click the map's zoom, the number (zoom level) is appropriately updated.
+    await test.step('Go to the first section again and then directly to the second one', async () => {
+        await goToTab(tabs[0]);
+        await goToTab(tabs[1]);
+    });
+
+    await test.step('Go to the "Explore Map" section', async () => {
+        await page.locator('#explore-map').getByRole('button', { name: 'Explore map' }).click();
+        await expect(page.locator('#deckgl-overlay')).toBeVisible();
+    });
+
+    await test.step('If we click the logo, we get back to the homepage.', async () => {
+        await page.locator('a').filter({ hasText: 'Hawai\'i HighwaysClimate Insights for Infrastructure' }).click();
+        await expect(await page.locator('#explore-map')).toBeVisible();
+    });
+
+    await test.step('Go back to the map ', async () => {
+        await page.locator('#explore-map').getByRole('button', { name: 'Explore map' }).click();
+        await expect(page.locator('#deckgl-overlay')).toBeVisible();
+    });
+
+    await test.step('If we click the map\'s zoom, the number (zoom level) should be appropriately updated', async () => {
+        const increaseButtonEl = page.getByRole('button', { name: 'Increase zoom' });
+        const zoomValueEl = increaseButtonEl.locator('xpath=./preceding-sibling::div/span');
+
+        const initialZoomValue = await zoomValueEl.textContent();
+        await increaseButtonEl.click();
+        const updatedZoomValue = await zoomValueEl.textContent();
+
+        expect(parseInt(initialZoomValue!) + 1).toEqual(parseInt(updatedZoomValue!));
+    });
 });
